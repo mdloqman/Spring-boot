dc60e12e82f680fc9afebdfb3d2566d9d5c2f535
mdloqman <80279814+mdloqman@users.noreply.github.com>
2022-02-16 23:42:47 -0800

    MainClass

--- src/main/java/com/task/startapp/TaskManagementApplication.java
+++ src/main/java/com/task/startapp/TaskManagementApplication.java
@@ -1,5 +1,4 @@
 package com.task.startapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
